3183e43a7fca67437eba7131debe98fd85d4b033
Robi <kocsisr@kkszki.hu>
2026-03-23 10:09:04 +0100

    egy kis soros fix

--- lynqo_frontend/src/index.js
+++ lynqo_frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/styles/global.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { LanguageProvider } from "./context/LanguageContext";
 import './i18n';
